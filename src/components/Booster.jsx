3bc1742e127cbb42de2cd17e63b4cb6935012890
Pekka Aalto <Pekka.Aalto@sasky.fi>
2023-05-22 13:25:42 +0300

    lisää Booster-komponentin

---
+++ src/components/Booster.jsx
@@ -0,0 +1,14 @@
+function Booster(props) {
+
+  // Poimitaan komponentille välitetty value-arvo
+  const value = props.value;
+  
+  return (
+    <div className="booster">
+      {value} lemon / click
+    </div>
+  );
+  
+}
+  
+export default Booster;
